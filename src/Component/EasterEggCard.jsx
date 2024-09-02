b5e30f1150df90df418c120844ec8ac47ddcf652
KhaledAlshathri <157881411+KhaledAlshathri@users.noreply.github.com>
2024-09-02 20:43:33 +0300

    changing the trophy format to svg for faster loading

--- src/Component/EasterEggCard.jsx
+++ src/Component/EasterEggCard.jsx
@@ -7,13 +7,13 @@ function EasterEggCard({ easterEgg }) {
   useEffect(() => {
      
     if(num === 1){
-        setTrophyType("../images/bronze-trophy.png");
+        setTrophyType("../images/bronze-trophy.svg");
         setNum(2)
     } else if(num === 2) {
-        setTrophyType("../images/silver-trophy.png");
+        setTrophyType("../images/silver-trophy.svg");
         setNum(3)
     } else if(num === 3){
-        setTrophyType("../images/gold-trophy.png");
+        setTrophyType("../images/gold-trophy.svg");
     }
     
   }, []);
@@ -23,7 +23,7 @@ function EasterEggCard({ easterEgg }) {
       <div className="easter-egg-card">
         {eggContent}
         <div>
-          <img
+          <img 
             className="trophy"
             src={trophyType}
             alt="trophy"
